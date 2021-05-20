23846613b4d7fb425ce0ae9f51d19d01c0a18dee
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2021-05-20 22:54:46 +0200

    solve hw2 task2

--- homework_2/task_1/task_1.cpp
+++ homework_2/task_1/task_1.cpp
@@ -1,6 +1,32 @@
+#include <cstdlib>
 #include <iostream>
+#include <random>
 
 int main() {
-  std::cout << "Hello clang-format\n";
-  return 0;
+  std::random_device rd;
+  const int max = 99;
+  std::uniform_int_distribution<int> dist(0, max);
+  int random = dist(rd);
+  int guess = -1;
+
+  std::cerr << "I think for some number...\n";
+  while (guess != random) {
+    std::cerr << "Make a guess!: ";
+
+    std::cin >> guess;
+    if (std::cin.fail()) {
+      std::cerr << "Error can't handle the input it is a not number.\n";
+      return EXIT_FAILURE;
+    }
+
+    if (guess < random) {
+      std::cerr << "The number is bigger..\n";
+    } else if (guess > random) {
+      std::cerr << "The number is smaller.\n";
+    }
+  }
+
+  std::cerr << "Yes!! " << random << " is the correct number!\n";
+
+  return EXIT_SUCCESS;
 }
